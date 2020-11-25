bad00f98995eede4c25acbcacea7da8bc6e55d86
theboys619 <theboys619@gmail.com>
2020-11-25 17:20:19 -0500

    updated folders and example

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include "includes/Json.hpp"
+#include "include/Json.hpp"
 
 int main(int argc, char** argv) {
   
   JSONObject x = JSON::readFromFile("./tests/test.json");
 
-  std::cout << x["why"][0]["are"].getString() << std::endl;
-  std::cout << x["why"][1].getString() << std::endl; // Should be 2
-  std::cout << x["why"][2].getString() << std::endl; // Should be 3
-  std::cout << x["why"][3].getInt() << std::endl; // Should be 4
+  // std::cout << x["why"][0]["are"].getString() << std::endl;
+  // std::cout << x["why"][1].getString() << std::endl; // Should be 2
+  // std::cout << x["why"][2].getString() << std::endl; // Should be 3
+  // std::cout << x["why"][3].getInt() << std::endl; // Should be 4
+  std::cout << x["place_url"].getString() << std::endl;
 
   JSON::writeToFile("./tests/test.json", x, 2);
 
